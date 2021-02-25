716dc1e8ed43a1bc0e31d5ebb2205e6b4ff119f6
kenjiprograming <iamkenji.programing@gmail.com>
2021-02-25 15:05:39 +0900

    imrpove layout for btn etc

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,20 +1,45 @@
-$(function () {
+jQuery(document).on('turbolinks:load', function() {
   var error1 = false;
-
-  $("input.user_search").blur(function() {
-    if ($(this).val() == "") {
-      $(this).css("background-color", "#FEF4F8");	
+  $('#form_controll').blur(function() {
+    if ($(this).val() == '') {
+      $(this).css('background-color', '#FEF4F8');	
       error1 = true;
     } else {
-      $(this).css("background-color", "#FaFEFF");
+      $(this).css('background-color', '#FaFEFF');
       error1 = false;
-    }
-  })
-
-  $("form").submit(function() {
+    };
+  });
+  $('#search_btn').click(function() {
     if (error1 == true) {
-      alert("最低1文字以上入力してください");
+      alert('最低1文字以上入力してください');
       return false;
-    }
+    };
   });
 });
+
+// $(function (){
+//   $("input#form_controll").hide();
+// });
+
+// $(function () {
+//   //バリデーション
+//   $("input#form_controll").on("blur", function () {
+//     let error;
+//     let value = $(this).val();
+//     if (value == "" || !value.match(/[^\s\t]/)) {
+//       error = true;
+//     }
+
+//     if (error) {
+//       //エラー時の処理
+//       alert('必須ですよ');
+//     }
+//   });
+// });
+
+// jQuery(document).on('turbolinks:load', function() {
+//   $('#email').hide();
+//   $('#search_btn').click(function () {
+//     $('#email').toggle();
+//   });
+// });
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 7 }, format: { with: VALID_PASSWORD_REGEX }
+
+
+
   has_many :posteds, dependent: :destroy
 
   has_many :relationships, dependent: :destroy
